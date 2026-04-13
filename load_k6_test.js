ab11a6e9fe52f409cdc5d9b0666a9cbc169f284f
heeraknp <heera@k-id.com>
2026-04-13 21:12:08 +0530

    cloud run

--- load_k6_test.js
+++ load_k6_test.js
@@ -3,14 +3,13 @@ import { check, sleep } from "k6";
 
 export let options = {
   stages: [
-    { duration: "30s", target: 10 },
-    { duration: "1m", target: 10 },
-    { duration: "30s", target: 0 },
+    { duration: "10s", target: 10 },
+    { duration: "10s", target: 10 },
+    { duration: "10s", target: 0 },
   ],
 };
-
 export default function () {
-  let res = http.get("https://quickpizza.grafana.com/");
+  let res = http.get("https://quickpizza.grafana.com");
   check(res, {
     "response status is 200": (r) => r.status === 200,
     "response time is < 500ms": (r) => r.timings.duration < 500,
