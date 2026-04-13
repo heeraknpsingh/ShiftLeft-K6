f9573232b5e1440cbecc9ffccd4b10fc777d3282
heeraknp <heera@k-id.com>
2026-04-13 21:16:34 +0530

    config changes again

--- load_test.js
+++ load_test.js
@@ -3,9 +3,9 @@ import { check, sleep } from "k6";
 
 export let options = {
   stages: [
-    { duration: "30s", target: 10 },
+    { duration: "10s", target: 10 },
     { duration: "1m", target: 10 },
-    { duration: "30s", target: 0 },
+    { duration: "10s", target: 0 },
   ],
 };
 
